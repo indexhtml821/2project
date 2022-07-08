d6c92041908bf0932d5b93395e17a3715ab8a704
indexhtml821 <isaacopine@gmail.com>
2022-07-08 15:34:06 -0600

    se añade test modifica nombre test

--- tests/tests_empleado_nomina.cpp
+++ tests/tests_empleado_nomina.cpp
@@ -19,7 +19,7 @@ namespace
     EXPECT_FLOAT_EQ(esperada, actual);
   }
 
-  TEST(Tests_Empleado_Nomina, Test_DevolverInfoEmpleado)
+  TEST(Tests_Empleado_Nomina, Test_InfoEmpleadoParaArchivo)
   {
     /// AAA
 
@@ -30,6 +30,22 @@ namespace
     empleadonomina.CalculoPago();
     string actual = empleadonomina.InfoEmpleadoParaArchivo();
     string esperada = "1 Denis Tyler 3720.623";
+
+    // Assert - valide los resultados
+    EXPECT_EQ(esperada, actual);
+  }
+
+  TEST(Tests_Empleado_Nomina, Test_InfoCompletaEmpleados)
+  {
+    /// AAA
+
+    // Arrange - configurar el escenario
+    EmpleadoNomina empleadonomina(1, "Denis", "Tyler", "denis_tyler@biz.com", 1, 1, 4000.67);
+
+    // Act - ejecute la operación`-
+    empleadonomina.CalculoPago();
+    string actual = empleadonomina.InfoCompletaEmpleados();
+    string esperada = "1 Denis Tyler denis_tyler@biz.com 1 1 4000.67 0.000";
 
     // Assert - valide los resultados
     EXPECT_EQ(esperada, actual);
